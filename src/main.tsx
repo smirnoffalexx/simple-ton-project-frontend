8b34c2417a336cbe1ed69c6a0fb5500989cf1818
smirnov.am16 <smirnov.am16@physics.msu.ru>
2024-06-29 11:53:36 +0300

    update README.md, manifest, vite.config.ts, upload icon

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
 const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+  "https://smirnoffalexx.github.io/simple-ton-project-frontend/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
